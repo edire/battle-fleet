d4bb16a4ba6eadf132dae054a274c9304c19462b
edire <az8641683@163.com>
2016-10-11 11:13:30 +0800

    修改

--- index.js
+++ index.js
@@ -7,32 +7,49 @@ function Board(x, y, size, direction) {
     this.realCard = getRealCard(x, y, size, direction);
 }
 
-function getRealCard (x, y, size, direction) {
+// 获取物体的位置
+function getRealCard(x, y, size, direction) {
     var arr = [],
         i;
     i = (direction == 'horizontal') ? x : y;
-    for (var a = i; i < size + a; i++) {
-        arr.push([i, y]);
-    }
+    for (var a = i; i < size + a; i++)
+        arr.push(createDot(x, y, i, direction));
     return arr;
 }
 
-function getUseCard (x, y, size, direction) {
+// 获取周围的点
+function getRoundCard(x, y, size, direction) {
     var arr = [],
         i;
     i = (direction == 'horizontal') ? x : y;
-    for (var a = i; i < size + a; i++) {
-        arr.push([i, y]);
-        arr.push([i, y + 1]);
-        arr.push([i, y - 1]);
+
+    for (var a = i; i - 1 <= size + a; i++) {
+        arr.push(getDot(x, y, i, direction));
     }
-    arr.push([x-1, y]);
-    arr.push([x-1, y+1]);
-    arr.push([x-1, y-1]);
-
-    arr.push([x+size+1, y]);
-    arr.push([x+size+1, y+1]);
-    arr.push([x+size+1, y-1]);
-
-    return arr;
+    return getSideDot(arr, direction);
 }
+
+// 根据x,y,i 获取占用点
+function getDot(x, y, i, direction) {
+    return (direction == 'horizontal') ? [i, y] : [x, i];
+}
+
+// 根据占用点获取两边的点
+function getSideDot(arr, direction) {
+    var newArr = [];
+    arr.forEach(function(d) {
+        if (direction == 'horizontal') {
+            newArr = newArr.concat([
+                [d[0], d[1] + 1],
+                [d[0], d[1] - 1]
+            ]);
+        } else {
+            newArr = newArr.concat([
+                [d[0] + 1, d[1]],
+                [d[0] - 1, d[1]]
+            ]);
+        }
+    });
+    return newArr;
+}
+
